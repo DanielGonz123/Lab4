bb2e5686dbf2136cea2622247851819764e423f5
DanielGonz123 <91735919+DanielGonz123@users.noreply.github.com>
2025-02-12 13:09:31 -0500

    Update Lab4.py

--- Lab4.py
+++ Lab4.py
@@ -23,9 +23,10 @@ def is_prime(n):
 def print_prime_factors(n):
     factors = []
     divisor = 2
+    original_n = n
     while n > 1:
         while n % divisor == 0:
             factors.append(divisor)
             n //= divisor
         divisor += 1
-    print(" * ".join(map(str, factors)))
+    print(f"{original_n} = {' * '.join(map(str, factors))}")
